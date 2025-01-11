f61f97549c2a29215076dc638f60b109a7d5ab5a
BrenoCRSilva <brenocrs@gmail.com>
2025-01-11 00:23:53 -0300

    feat: added formatters

--- brenocrs/remap.lua
+++ brenocrs/remap.lua
@@ -1,5 +1,5 @@
 vim.keymap.set("n", "<leader>pv", ":Ex<CR>")
-vim.keymap.set("n", "<leader>w", ":w<CR>")
+vim.keymap.set("n", "<leader>s", ":w<CR>")
 vim.keymap.set("n", "<leader>q", ":q!<CR>")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
--- plugins/mason.lua
+++ plugins/mason.lua
@@ -29,11 +29,10 @@ return {
 
     mason_tool_installer.setup({
       ensure_installed = {
-        "prettier", -- prettier formatter
         "stylua", -- lua formatter
-        "isort", -- python formatter
-        "black", -- python formatter
-        "pylint",
+        "golines",
+        "goimports",
+        "gomodifytags"
       },
     })
   end,
