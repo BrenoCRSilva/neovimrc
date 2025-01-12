8808d7dff6ca2f807282aebda284f4768454b5d8
BrenoCRSilva <brenocrs@gmail.com>
2025-01-11 21:24:37 -0300

    fix: linters for go and lua

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -1,40 +1,40 @@
 return {
-    "williamboman/mason-lspconfig.nvim",
-  dependencies = {
-    "williamboman/mason.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-  },
-  config = function()
-    local mason_lspconfig = require("mason-lspconfig")
-    local mason = require("mason")
-    local mason_tool_installer = require("mason-tool-installer")
+	"williamboman/mason-lspconfig.nvim",
+	dependencies = {
+		"williamboman/mason.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		local mason_lspconfig = require("mason-lspconfig")
+		local mason = require("mason")
+		local mason_tool_installer = require("mason-tool-installer")
 
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-      },
-    })
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
 
-    mason_lspconfig.setup({
-      ensure_installed = {
-        "lua_ls",
-        "pyright",
-        "gopls"
-      },
-    })
+		mason_lspconfig.setup({
+			ensure_installed = {
+				"lua_ls",
+				"pyright",
+				"gopls",
+			},
+		})
 
-    mason_tool_installer.setup({
-      ensure_installed = {
-        "stylua", -- lua formatter
-        "gofumpt",
-        "golines",
-        "goimports",
-        "gomodifytags"
-      },
-    })
-  end,
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"stylua", -- lua formatter
+				"gofumpt",
+				"golines",
+				"goimports",
+				"gomodifytags",
+			},
+		})
+	end,
 }
